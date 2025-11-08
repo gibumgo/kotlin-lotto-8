bba67585c8fae812f8e921cccaf415015f9773df
gibumgo <gibumgo@gmail.com>
2025-11-08 11:29:08 +0900

    feat: 금액 범위 유효성 기능 추가

--- src/main/kotlin/lotto/domin/Money.kt
+++ src/main/kotlin/lotto/domin/Money.kt
@@ -1,5 +1,21 @@
 package lotto.domin
 
+import lotto.constant.LottoMessages
+import lotto.constant.LottoRules
+
 data class Money (private val money: Int) {
+    init {
+        validate(money)
+    }
+
+    private fun validate(money: Int) {
+        if (isInvalidAmount(money)) {
+            throw IllegalArgumentException(LottoMessages.moneyError(LottoRules.MONEY_PER_LOTTO))
+        }
+    }
+
+    private fun isInvalidAmount(money: Int): Boolean {
+        return money < 0 || money % 1000 != 0
+    }
 
 }
